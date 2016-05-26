1a7e1a6bdb1a16e55715cd870ed06efec7410e90
Erwin Poeze <erwin@mope.mobi>
2016-05-26 07:36:56 +0200

    adds multiple-ttl checks

--- index.js
+++ index.js
@@ -3,61 +3,87 @@ const net = require('net');
 
 module.exports = (function() {
 
-  function Howru(options) {
-    this.type = options.type || 'http';
-    if (this.type == 'http') {
-      this.route = options.route || '/health';
-      this.port = options.port || 6999;
-    } else if (this.type == 'tcp') {
-      this.port = options.port || 6999;
-    }
-    this.status = 200;
-    this.server = null;
-  }
-
-  Howru.prototype.status = function() {
-    return this.status;
-  }
-
-  Howru.prototype.stop = function() {
-    if (this.type == 'http') {
-      this.server.close();
-    } else if (this.type == 'tcp') {
-      this.server.close();
-    }
-  }
-
-  Howru.prototype.died = function() {
-    this.status = 500;
-  }
-
-  Howru.prototype.start = function() {
-
-    if (this.type == 'http') {
-      this.server = http.createServer((req, res) => {
-        if (req.url == this.route) {
-          res.writeHead(this.status, {'Content-Type': 'text/plain'});
-          res.end();
+    function Howru(options) {
+        this.type = options.type || 'http';
+        if (this.type == 'http') {
+            this.route = options.route || '/health';
+            this.port = options.port || 6999;
+        } else if (this.type == 'tcp') {
+            this.port = options.port || 6999;
+        } else if (this.type == 'ttl') {
+            this.host = options.host || 'localhost';
+            this.port = options.port || 6999;
+            this.path = options.path || '/';
+            this.interval = options.interval || 10000;
         }
-      });
-
-    } else if (this.type == 'tcp') {
-      this.server = net.createServer()
-      this.server.on('connection', (sock) => {
-        sock.on('end', () => {});
-        sock.on('data', (data) => {
-          sock.write(String(this.status));
-        })
-      });
-    } else {
-      throw new Error('not implemented yet');
-      // push health status
+        this.status = 200;
+        this.server = null;
     }
 
-    this.server.listen(this.port);
+    Howru.prototype.status = function() {
+        return this.status;
+    }
 
-  }
+    Howru.prototype.stop = function() {
+        if (this.type == 'http') {
+            this.server.close();
+        } else if (this.type == 'tcp') {
+            this.server.close();
+        }
+    }
 
-  return Howru;
+    Howru.prototype.died = function() {
+        this.status = 500;
+    }
+
+    Howru.prototype.start = function() {
+
+        if (this.type == 'http') {
+            this.server = http.createServer((req, res) => {
+                if (req.url == this.route) {
+                    res.writeHead(this.status, {'Content-Type': 'text/plain'});
+                    res.end();
+                }
+            });
+            this.server.listen(this.port);
+
+        } else if (this.type == 'tcp') {
+            this.server = net.createServer()
+                this.server.on('connection', (sock) => {
+                    sock.on('end', () => {});
+                    sock.on('data', (data) => {
+                        sock.write(String(this.status));
+                    })
+                });
+            this.server.listen(this.port);
+
+        } else if (this.type == 'ttl') {
+            let req = http.request({
+                host: this.url,
+                port: this.port,
+                method: 'PUT',
+                path: this.path,
+                headers: {
+                    'Content-Type': 'application/x-text'
+                }
+            });
+
+            req.write('OK');
+            setInterval(() => {
+                req.write(String(this.status));
+            }, this.interval);
+            req.on('error', (e) => { 
+                req.end();
+                console.log(`problem with request: ${e.message}`);
+            });
+
+        } else {
+            throw new Error('not implemented yet');
+            // push health status
+        }
+
+    }
+
+    return Howru;
 
 }());
--- test/test.connections.js
+++ test/test.connections.js
@@ -6,185 +6,251 @@ const Howru = require('..');
 
 describe('Connections', function() {
 
-  describe('HTTP', function() {
+    describe('HTTP', function() {
 
-    it('reject premature connection', function(done) {
+        it('reject premature connection', function(done) {
 
-      const health = new Howru({
-        type: 'http',
-        route: '/health'
-      });
+            const health = new Howru({
+                type: 'http',
+                route: '/health'
+            });
 
-      var req = http.request({path: '/health', port: 6999, method: 'POST'}, (res) => {});
-      req.on('error', (err) => {
-        assert.equal(err.code, 'ECONNREFUSED');
-        done();
-      });
+            var req = http.request({path: '/health', port: 6999, method: 'POST'}, (res) => {});
+            req.on('error', (err) => {
+                assert.equal(err.code, 'ECONNREFUSED');
+                done();
+            });
+
+        });
+
+        xit('ignore unknown url route', function(done) {
+
+            const health = new Howru({
+                type: 'http',
+                route: '/health'
+            });
+
+            health.start();
+
+            var req = http.request({path: '/fail', port: 6999, method: 'POST'}, (res) => {
+                health.stop();
+                done();
+            });
+
+        });
+
+        it('responds with 200', function(done) {
+
+            const health = new Howru({
+                type: 'http',
+                route: '/health'
+            });
+
+            health.start();
+
+            var req = http.request({path: '/health', port: 6999, method: 'POST'}, (res) => {
+                assert.equal(res.statusCode, 200);
+                health.stop();
+                done();
+            });
+
+            req.write('200');
+
+        });
+
+        it('responds with 500', function(done) {
+
+            const health = new Howru({
+                type: 'http',
+                route: '/health'
+            });
+
+            health.start();
+            health.died();
+
+            var req = http.request({path: '/health', port: 6999, method: 'POST'}, (res) => {
+                assert.equal(res.statusCode, 500);
+                health.stop();
+                done();
+            });
+
+            req.write(' ');
+        });
+    });
+
+    describe('tcp', function() {
+
+        it('responds with 200', function(done) {
+            const net = require('net');
+
+            const health = new Howru({
+                type: 'tcp',
+                port: 6999
+            });
+
+            health.start();
+
+            const client = net.createConnection({port: 6999}, () => {
+                client.write('oke?');
+            });
+
+            client.on('data', (data) => {
+                assert.equal(parseInt(data, 10), 200);
+                health.stop();
+                done();
+            });
+        });
+
+        it('responds with 500', function(done) {
+            const net = require('net');
+
+            const health = new Howru({
+                type: 'tcp',
+                port: 6999
+            });
+
+            health.start();
+            health.died();
+
+            const client = net.createConnection({port: 6999}, () => {
+                client.write('oke?');
+            });
+
+            client.on('data', (data) => {
+                assert.equal(parseInt(data, 10), 500);
+                health.stop();
+                done();
+            });
+        });
 
     });
 
-    xit('ignore unknown url route', function(done) {
+    describe('ttl', function() {
 
-      const health = new Howru({
-        type: 'http',
-        route: '/health'
-      });
+        it('receives a 200 responds within ttl', function(done) {
 
-      health.start();
+            const ttl = 1000;
+            const startTime = Date.now();
 
-      var req = http.request({path: '/fail', port: 6999, method: 'POST'}, (res) => {
-        health.stop();
-        done();
-      });
+            const server = http.createServer(function(req, res) {
+                res.end();
+            });
+            server.on('request', function(req, res) {
+                console.log('receiving');
+                assert.ok(Date.now() - startTime < ttl);
+                health.stop();
+                server.close();
+                done();
+            });
+            server.listen(7000, 'localhost')
 
+                const health = new Howru({
+                    type: 'ttl',
+                    port: 7000,
+                    interval: 100
+                });
+
+            health.start();
+        });
     });
 
-    it('responds with 200', function(done) {
-
-      const health = new Howru({
-        type: 'http',
-        route: '/health'
-      });
-
-      health.start();
-
-      var req = http.request({path: '/health', port: 6999, method: 'POST'}, (res) => {
-        assert.equal(res.statusCode, 200);
-        health.stop();
-        done();
-      });
-
-      req.write('200');
-
-    });
-
-    it('responds with 500', function(done) {
-
-      const health = new Howru({
-        type: 'http',
-        route: '/health'
-      });
-
-      health.start();
-      health.died();
-
-      var req = http.request({path: '/health', port: 6999, method: 'POST'}, (res) => {
-        assert.equal(res.statusCode, 500);
-        health.stop();
-        done();
-      });
-
-      req.write(' ');
-    });
-  });
-
-  describe('tcp', function() {
-
-    it('responds with 200', function(done) {
-      const net = require('net');
-
-      const health = new Howru({
-        type: 'tcp',
-        port: 6999
-      });
-
-      health.start();
-
-      const client = net.createConnection({port: 6999}, () => {
-        client.write('oke?');
-      });
-
-      client.on('data', (data) => {
-        assert.equal(parseInt(data, 10), 200);
-        health.stop();
-        done();
-      });
-    });
-
-    it('responds with 500', function(done) {
-      const net = require('net');
-
-      const health = new Howru({
-        type: 'tcp',
-        port: 6999
-      });
-
-      health.start();
-      health.died();
-
-      const client = net.createConnection({port: 6999}, () => {
-        client.write('oke?');
-      });
-
-      client.on('data', (data) => {
-        assert.equal(parseInt(data, 10), 500);
-        health.stop();
-        done();
-      });
-    });
-
-  });
 });
 
 describe('Multiple checks', function() {
 
-  describe('HTTP', function() {
+    describe('HTTP', function() {
 
-    it('5 good healths in a row', function(done) {
+        it('5 good healths in a row', function(done) {
 
-      const health = new Howru({
-        type: 'http',
-        route: '/health',
-        port: 6999
-      });
+            const health = new Howru({
+                type: 'http',
+                route: '/health',
+                port: 6999
+            });
 
-      health.start();
+            health.start();
 
-      async.times(5, function(n, next) {
-        var req = http.request({path: '/health', port: 6999, method: 'POST'}, (res) => {
-          next(null, res.statusCode)
+            async.times(5, function(n, next) {
+                var req = http.request({path: '/health', port: 6999, method: 'POST'}, (res) => {
+                    next(null, res.statusCode)
+                });
+                req.write(' ');
+            }, function(err, result) {
+                assert.equal(result.length, 5);
+                assert.deepEqual(result, [200,200,200,200,200]);
+                health.stop();
+                done();
+            });
+
         });
-        req.write(' ');
-      }, function(err, result) {
-        assert.equal(result.length, 5);
-        assert.deepEqual(result, [200,200,200,200,200]);
-        health.stop();
-        done();
-      });
-
     });
-  });
 
-  describe('TCP', function() {
+    describe('TCP', function() {
 
-    it('5 good healths in a row', function(done) {
+        it('5 good healths in a row', function(done) {
 
-      const health = new Howru({
-        type: 'tcp',
-        port: 6999
-      });
-      let counter = 0;
+            const health = new Howru({
+                type: 'tcp',
+                port: 6999
+            });
+            let counter = 0;
 
-      health.start();
+            health.start();
 
-      const client = net.connect({port: 6999}, () => {
-        async.timesSeries(5, (n, next) => {
-          client.write('ke?');
-          setTimeout(next, 1);
-        })
-      });
+            const client = net.connect({port: 6999}, () => {
+                async.timesSeries(5, (n, next) => {
+                    client.write('oke?');
+                    setTimeout(next, 1);
+                })
+            });
 
-      client.on('data', (data) => {
-        counter++;
-        if (counter == 5) {
-          assert.ok(true);
-          health.stop();
-          done();
-        }
-      });
+            client.on('data', (data) => {
+                counter++;
+                if (counter == 5) {
+                    assert.ok(true);
+                    health.stop();
+                    done();
+                }
+            });
 
-      client.on('end', () => console.log('ended'));
+            client.on('end', () => console.log('ended'));
 
+        });
     });
-  });
+
+    describe('ttl', function() {
+
+        it('5 good healths in a row', function(done) {
+
+            const ttl = 1000;
+            const startTime = Date.now();
+
+            const health = new Howru({
+                type: 'ttl',
+                port: 7000,
+                interval: 100
+            });
+
+            health.start();
+            let counter = 0;
+
+            const server = http.createServer(function(req, res) {
+                //res.end();
+            });
+
+            server.on('request', function(req, res) {
+                req.on('data', (chunk) => {
+                    counter++;
+                    assert.ok(Date.now() - startTime < ttl);
+                    if (counter == 5) {
+                        health.stop();
+                        server.close();
+                        done();
+                    }
+                });
+            });
+            server.listen(7000)
+
+        });
+    });
 });
